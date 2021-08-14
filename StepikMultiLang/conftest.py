5da9ac5ad011efa3371a43ad9f1572ccc1169ff2
White_Rabbit <lacrimozzy989wr>
2021-08-15 01:26:41 +0300

    edit lesson

--- StepikMultiLang/conftest.py
+++ StepikMultiLang/conftest.py
@@ -2,11 +2,11 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='ru',
-                     help="Choose language")
+    parser.addoption('--browser_name', action='store', default='chrome', help="Выбор браузера: chrome или firefox")
+    parser.addoption('--language', action='store', default='ru', help="выбор языка")
+
 
 @pytest.fixture(scope='function')
 def browser(request):
@@ -14,16 +14,60 @@ def browser(request):
     language = request.config.getoption('language')
 
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
+        print(f'\nЗапуск {browser_name} для теста...')
         browser = webdriver.Chrome()
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+    elif browser_name == 'firefox':
+        print(f'\nЗапуск {browser_name} для теста...')
         browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('--browser_name должно быть chrome или firefox')
+    yield browser
+    print('\nЗакрытие браузера...')
+    browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help="Выбор браузера: Chrome или Firefox")
+    parser.addoption('--language', action='store', default='ru', help="Выбор языка")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption('language')
+
+    if browser_name == 'Сhrome':
+        print(f'\nЗапуск {browser_name} для теста..')
+        browser = webdriver.chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.chrome(options=options)
+    elif browser_name == 'firefox':
+        print(f'\nЗапуск {browser_name} для теста..')
+        browser = webdriver.firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
+    print('\nЗакрытие браузера...')
     browser.quit()
+'''
--- StepikMultiLang/test_items.py
+++ StepikMultiLang/test_items.py
@@ -1,9 +1,10 @@
 import time
 
+
 def test_button_add_to_basket_exist_on_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(2)
+    time.sleep(3)
     button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
     message = button.text
-    assert message == 'Ajouter au panier'
+    assert message == 'Añadir al carrito'
