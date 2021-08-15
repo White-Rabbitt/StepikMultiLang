f66048492dc0500ffde7383d6bf61108757df481
White_Rabbit <lacrimozzy989wr>
2021-08-15 16:03:50 +0300

    edit lesson

--- StepikMultiLang/test_items.py
+++ StepikMultiLang/test_items.py
@@ -4,7 +4,7 @@ import time
 def test_button_add_to_basket_exist_on_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
     message = button.text
     assert message == 'Añadir al carrito'
